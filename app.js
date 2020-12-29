d8c66f753b879a5be2d2b673ef844717bed433ed
Viktor Marchuk <dayverr87@gmail.com>
2020-12-29 15:58:11 +0200

    add: secrets

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    facebookId: String
+    facebookId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMangoose);
@@ -87,11 +88,15 @@ app.get("/register", function(req, res){
 })
 
 app.get("/secrets", function(req, res){
-    if(req.isAuthenticated()){
-        res.render("secrets");
-    } else {
-        res.redirect("/login");
-    }
+    User.find({"secret": {$ne: null}}, function(err, founded){
+        if(err){
+            console.log(err);
+        } else {
+            if(founded){
+                res.render("secrets", {userWithSecrets: founded});
+            }
+        }
+    });
 });
 
 app.get('/auth/facebook/secrets',
@@ -104,6 +109,14 @@ app.get('/auth/facebook/secrets',
 app.get("/logout", function(req, res){
     req.logout();
     res.redirect("/");
+});
+
+app.get("/submit", function (req,res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
 })
 
 app.post("/register", function(req, res){
@@ -135,9 +148,23 @@ app.post("/login", function(req, res){
             })
         }
     })
-
-
 });
+
+app.post("/submit", function(req,res){
+    const submitedSecret = req.body.secret;
+    User.findById(req.user.id, function(err, founded){
+        if(err){
+            console.log(err);
+        } else {
+            if (founded){
+                founded.secret = submitedSecret;
+                founded.save(function(){
+                    res.redirect("/secrets")
+                })
+            }
+        }
+    })
+})
 
 
 app.listen(3000, function(){
